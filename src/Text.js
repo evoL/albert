75d5647b004990ffe9cc40c0cbb0848dced034a8
Rafał Hirsz <rafal@hirsz.co>
2019-01-04 19:38:31 +0100

    Reimplement text formatting in Text
    It turns out that there's a <tspan> element that does exactly what we wanted.
    FormattedText is now deprecated in favor of Text.

--- src/Text.js
+++ src/Text.js
@@ -2,11 +2,15 @@ import { Expression } from "cassowary";
 import { createElement, omit, withTemporarySvg } from "./utils";
 import { variable } from "./helpers";
 
+function createSlice(start, end, attributes) {
+  return { start, end, attributes };
+}
+
 export default class Text {
   constructor(text, attributes = {}) {
     this.text_ = text;
     this.attributes_ = omit(attributes, ["x", "y", "font-size"]);
-    this.contextAttributes_ = {};
+    this.slices_ = []; // By default there will be no spans
 
     this.x = variable("x", attributes.x);
     this.y = variable("y", attributes.y);
@@ -34,12 +38,7 @@ export default class Text {
 
   setText(text) {
     this.text_ = text;
-    this.adjustDimensions_();
-    return this;
-  }
-
-  setContext(context) {
-    this.contextAttributes_ = context;
+    this.slices_ = [];
     this.adjustDimensions_();
     return this;
   }
@@ -48,12 +47,99 @@ export default class Text {
     return new Expression(this.fontSize).times(multiplier);
   }
 
-  render(useContext = false) {
-    const attributes = useContext
-      ? Object.assign({}, this.contextAttributes_, this.attributes_)
-      : this.attributes_;
-    const el = createElement("text", attributes);
-    el.appendChild(document.createTextNode(this.text_));
+  format(start, end, attributes = {}) {
+    if (start >= end) {
+      throw new Error(
+        `Invalid start or end passed to Text.format(): (${start}, ${end})`
+      );
+    }
+
+    if ("font-family" in attributes) {
+      throw new Error(
+        "Changing the font-family for parts of Text is not supported"
+      );
+    }
+    if (
+      "font-size" in attributes &&
+      !/(%|em|ex|ch)$/.test(attributes["font-size"])
+    ) {
+      throw new Error(
+        "Changing the font-size is only supported for relative units (%, em, ex, ch)"
+      );
+    }
+
+    for (let i = 0; i < this.slices_.length; i++) {
+      const slice = this.slices_[i];
+      if (end <= slice.start) {
+        // Does not overlap from the left side:
+        // Add before the existing slice.
+        this.slices_.splice(i, 0, createSlice(start, end, attributes));
+        return this;
+      }
+      if (start >= slice.end) {
+        // Does not overlap on the right side:
+        // Check with the next slice.
+        continue;
+      }
+
+      // Overlaps:
+      // Split into max three and replace the current.
+      const newSlices = [];
+      if (start < slice.start) {
+        newSlices.push(createSlice(start, slice.start, attributes));
+      } else if (start > slice.start) {
+        newSlices.push(createSlice(slice.start, start, slice.attributes));
+      }
+
+      newSlices.push(
+        createSlice(
+          Math.max(start, slice.start),
+          Math.min(end, slice.end),
+          Object.assign({}, slice.attributes, attributes)
+        )
+      );
+
+      if (end < slice.end) {
+        newSlices.push(createSlice(end, slice.end, slice.attributes));
+      }
+
+      this.slices_.splice(i, 1, ...newSlices);
+
+      if (end > slice.end) {
+        start = slice.end;
+        i += newSlices.length - 1;
+        continue;
+      }
+      return this;
+    }
+
+    // Didn't find a slice to merge with:
+    // Add a new one at the end.
+    this.slices_.push(createSlice(start, end, attributes));
+    return this;
+  }
+
+  formatRegexp(regexp, attributes = {}) {
+    if (typeof regexp === "string") {
+      regexp = new RegExp(regexp);
+    }
+
+    let matches;
+    let stop = false;
+    while (!stop && (matches = regexp.exec(this.text_)) !== null) {
+      this.format(matches.index, matches.index + matches[0].length, attributes);
+      stop = !regexp.global;
+    }
+    return this;
+  }
+
+  render() {
+    const el = createElement("text", this.attributes_);
+    if (this.slices_.length) {
+      this.renderSlices_(el);
+    } else {
+      el.appendChild(document.createTextNode(this.text_));
+    }
 
     el.setAttributeNS(null, "x", this.x.value);
     el.setAttributeNS(null, "y", this.y.value);
@@ -62,8 +148,29 @@ export default class Text {
     return el;
   }
 
+  renderSlices_(el) {
+    let position = 0;
+    for (const { start, end, attributes } of this.slices_) {
+      if (position < start) {
+        el.appendChild(
+          document.createTextNode(this.text_.slice(position, start))
+        );
+        position = start;
+      }
+
+      const span = createElement("tspan", attributes);
+      span.textContent = this.text_.slice(position, end);
+      el.appendChild(span);
+      position = end;
+    }
+
+    if (position < this.text_.length) {
+      el.appendChild(document.createTextNode(this.text_.slice(position)));
+    }
+  }
+
   adjustDimensions_() {
-    const el = this.render(true);
+    const el = this.render();
     withTemporarySvg(el, text => {
       const bbox = text.getBBox();
 
